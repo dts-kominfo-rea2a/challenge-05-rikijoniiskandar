451337727ca284c062b8476c1b66803efa45a8f9
Riki Joni Iskandar <rikijoniiskandar97@gmail.com>
2022-06-06 13:31:02 +0700

    feat: menyelesaikan function sorter, sortAscending dan sortDescending

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrData, fnSort) => {
+
+  dataArray = fnSort(arrData)
+
+  let data = []
+  for (count = 0; count < dataArray.length; count++) {
+    data.push(count + 1 + ". " + dataArray[count])
+  }
+  return data
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrData) => {
+  let data = arrData.sort()
+  return data
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrData) => {
+  let data = arrData.sort().reverse()
+  return data
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
